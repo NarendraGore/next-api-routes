cbd21dc96e2e6150e5330778dccaf341302e274c
NarendraGore <ngore4981@gmail.com>
2026-02-09 18:06:37 +0530

    Static DB File

---
+++ src/utils/DB.ts
@@ -0,0 +1,7 @@
+export const user = [
+    {id:1, name:"ABC", age:30, city:"Pune"},
+    {id:2, name:"DEF", age:25, city:"Mumbai"},
+    {id:3, name:"GHI", age:35, city:"Delhi"},
+    {id:4, name:"JKL", age:28, city:"Bangalore"},
+    {id:5, name:"MNO", age:32, city:"Chennai"},
+]
