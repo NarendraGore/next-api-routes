5049fe1747f53130cd3efe71c4541126770b64d7
NarendraGore <ngore4981@gmail.com>
2026-02-10 18:40:18 +0530

    API PUT Method

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -15,3 +15,18 @@ export async function GET(
   return NextResponse.json(
     filteredData.length == 0 ? {result: "No user found", success:false } : {result:filteredData[0], success:true}, { status: 200 });
 }
+
+
+export async function PUT(request: Request,   { params }: { params: { id: string } },
+ ) {
+  const payload = await request.json();
+ const {id} = await params;
+  const userID = id;
+  payload.id = userID;
+  console.log("User ID from URL", userID);
+  console.log("Payload from PUT API", payload);
+if(!payload.name||!payload.age||!payload.city){
+    return NextResponse.json({error:"request data is not valid", success:false},{status:400})
+}
+  return NextResponse.json({result:payload, success:true},{status:200})
+}
