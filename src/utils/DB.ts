b4279fa0b05a05ae20160d2f12cf958ad89b4b18
NarendraGore <ngore4981@gmail.com>
2026-02-11 18:50:58 +0530

    static data changed

--- src/utils/DB.ts
+++ src/utils/DB.ts
@@ -1,7 +1,7 @@
 export const user = [
-    {id:1, name:"ABC", age:30, city:"Pune"},
-    {id:2, name:"DEF", age:25, city:"Mumbai"},
-    {id:3, name:"GHI", age:35, city:"Delhi"},
-    {id:4, name:"JKL", age:28, city:"Bangalore"},
-    {id:5, name:"MNO", age:32, city:"Chennai"},
+    {id:1, name:"Rohit", age:30, city:"Pune"},
+    {id:2, name:"Agnivesh", age:25, city:"Mumbai"},
+    {id:3, name:"Rushikesh", age:35, city:"Delhi"},
+    {id:4, name:"Prathmesh", age:28, city:"Bangalore"},
+    {id:5, name:"Sagar", age:32, city:"Chennai"},
 ]
